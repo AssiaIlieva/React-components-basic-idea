1b1fae647a2ae36061fa7e52cb14ac45f9ebb6ef
AssiaIlieva <ac17@abv.bg>
2024-07-02 10:06:04 +0300

    Add demo of creating React elements and components with JS and JSX

--- jsx-demo/src/app.js
+++ jsx-demo/src/app.js
@@ -15,6 +15,47 @@ const headingReactSectionElement = (
 </header>
 );
 
+// Create component without JSX
+
+function Main(props){
+    return React.createElement(
+        'main',
+        {},
+        React.createElement('h3', {}, props.title),
+        React.createElement(
+            'ul',
+            {},
+            React.createElement(
+                'li',
+                {},
+                'The Matrix',
+            ),
+            React.createElement(
+                'li',
+                {},
+                'Man of steel'
+            )
+        )
+    )
+};
+
+// const siteContent = React.createElement(
+//     'div',
+//     {},
+//     headingReactSectionElement,
+//     React.createElement(
+//         Main,
+//         {title: 'Best movies'}
+//     )
+// )
+
+const siteContentJSX = (
+    <div>
+        {headingReactSectionElement}
+        <Main title="Best Movies" />
+    </div>
+)
+
 // Render content
-rootReactElement.render(headingReactSectionElement);
+rootReactElement.render(siteContentJSX);
 // npx babel --watch src --out-dir build --presets react-app/prod
