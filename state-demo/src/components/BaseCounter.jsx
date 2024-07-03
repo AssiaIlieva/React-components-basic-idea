da7ab1931db3a59b5ee769f27f7465bc8019867e
AssiaIlieva <ac17@abv.bg>
2024-07-03 13:41:28 +0300

    Add BaseCounter - events Demo

---
+++ state-demo/src/components/BaseCounter.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+
+export default function Counter() {
+    const [count, setCount] = useState(0);
+
+    const incrementButtonClickHandler = () => {
+        setCount(oldCount => oldCount + 1);
+    };
+
+    const resetButtonClickHandler = () => {
+        setCount(0);
+    };
+
+    const decrementButtonClickHandler = () => {
+        setCount(oldCount => oldCount - 1);
+    };
+
+    return (
+        <>
+            <h2>Counter</h2>
+
+            <p>{count}</p>
+
+            <button onClick={decrementButtonClickHandler}>-</button>
+
+            <button onClick={incrementButtonClickHandler}>+</button>
+
+            <button onClick={resetButtonClickHandler}>Reset</button>
+        </>
+    );
+}
