97643c537f537fdd3dfd4e3519ce2a702158b001
AssiaIlieva <ac17@abv.bg>
2024-07-03 13:40:21 +0300

    Add App.js

---
+++ state-demo/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Timer from './components/Timer';
+import BaseCounter from './components/BaseCounter'; 
+
+function App() {
+
+  return (
+    <>
+    <Timer/>
+
+    <BaseCounter/>
+    </>
+  );
+}
+
+export default App;
